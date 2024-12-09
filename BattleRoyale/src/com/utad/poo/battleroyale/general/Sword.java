b10b1c172d27afe49badb87659163e14a7a61c55
Zorutiss <vicgalsan1@gmail.com>
2024-12-09 23:51:40 +0100

    Weapons

---
+++ BattleRoyale/src/com/utad/poo/battleroyale/general/Sword.java
@@ -0,0 +1,14 @@
+package com.utad.poo.battleroyale.general;
+
+public class Sword extends Weapons{
+	
+	private Double damage;
+	private Integer level;
+	
+	public static final String DEF_NAME = "Sword";
+
+	
+	public Sword(Double damage, Integer level) {
+		
+	}
+}
