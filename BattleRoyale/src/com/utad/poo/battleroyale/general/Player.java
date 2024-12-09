eed593f33cf81f769edd5c3b240e6c4d6c1aa08e
Jaime744 <jaim3cr94@gmail.com>
2024-12-09 23:55:35 +0100

    mario

--- BattleRoyale/src/com/utad/poo/battleroyale/general/Player.java
+++ BattleRoyale/src/com/utad/poo/battleroyale/general/Player.java
@@ -1,4 +1,4 @@
-package com.utad.poo.battleroyale.ui;
+package com.utad.poo.battleroyale.general;
 
 public class Player {
 	
@@ -7,18 +7,17 @@ public class Player {
 	protected Integer health;
 	public static final String DEF_NAME="Player";
 	public static final String DEF_CLASS_NAME="Prisoner";
+	public static final Integer DEF_HEALTH=100;
 	
 	public Player(){
-		
+		this(Player.DEF_NAME,Player.DEF_CLASS_NAME,Player.DEF_HEALTH);
 	}
 	public Player(String name,String className,Integer health){
 		this.health=health;
 		this.name=name;
 		this.className=className;
 	}
-
-
-
+	
 	public String getName() {
 		return this.name;
 	}
@@ -29,37 +28,26 @@ public class Player {
 		this.name = name;
 	}
 
-
-
 	public String getClassName() {
 		return this.className;
 	}
-
-
 
 	public void setClassName(String className) {
 		this.className = className;
 	}
 
-
-
 	public Integer getHealth() {
 		return this.health;
 	}
-
-
 
 	public void setHealth(Integer health) {
 		this.health = health;
 	}
 
-
-
 	@Override
 	public String toString() {
 		return "Player [name= " + this.name + ", class= " + this.className  + ", health= " + this.className + " ]";
 	}
-	
 	
 
 }
