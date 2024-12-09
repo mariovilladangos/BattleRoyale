fe3e8e6b0a7e7a2640838d8f229d4cb1fe8f1192
Jaime744 <jaim3cr94@gmail.com>
2024-12-09 23:31:56 +0100

    player
    lo pongo todos los atributos en ingles??

---
+++ BattleRoyale/src/com/utad/poo/battleroyale/ui/Player.java
@@ -0,0 +1,17 @@
+package com.utad.poo.battleroyale.ui;
+
+public class Player {
+	
+	private String name;
+	private String clase;
+	private Integer vida;
+	
+	
+	
+	public Player(String name,String clase,Integer vida){
+		
+		//hola chikis 
+	}
+	
+
+}
