914d35d2d5a1469afec5b0cab7d1687b035ef0c6
Zorutiss <vicgalsan1@gmail.com>
2024-12-10 00:49:10 +0100

    Weapon
    Weapon, Sword, Spear y Claymore

--- BattleRoyale/src/com/utad/poo/battleroyale/general/Weapon.java
+++ BattleRoyale/src/com/utad/poo/battleroyale/general/Weapon.java
@@ -5,5 +5,29 @@ public class Weapon {
 	protected Double damage;
 	protected Integer level;
 	
+	public static final Double DEF_DAMAGE = 10.0;
+	public static final Integer DEF_LEVEL = 1;
+	
+	//Getters and setters
+	public Double getDamage() {
+		return damage;
+	}
+
+	public void setDamage(Double damage) {
+		this.damage = damage;
+	}
+
+	public Integer getLevel() {
+		return level;
+	}
+
+	public void setLevel(Integer level) {
+		this.level = level;
+	}
+	//ToString
+	@Override
+	public String toString() {
+		return "Weapon [damage=" + damage + ", level=" + level + "]";
+	}
 	
 }
