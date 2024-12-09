58ac017209d62c85a1766fa12e81800d25cdcaa9
Jaime744 <jaim3cr94@gmail.com>
2024-12-09 23:51:56 +0100

    putitas

--- BattleRoyale/src/com/utad/poo/battleroyale/ui/Player.java
+++ BattleRoyale/src/com/utad/poo/battleroyale/ui/Player.java
@@ -2,16 +2,64 @@ package com.utad.poo.battleroyale.ui;
 
 public class Player {
 	
-	private String name;
-	private String clase;
-	private Integer vida;
+	protected String name;
+	protected String className;
+	protected Integer health;
+	public static final String DEF_NAME="Player";
+	public static final String DEF_CLASS_NAME="Prisoner";
 	
-	
-	
-	public Player(String name,String clase,Integer vida){
+	public Player(){
 		
-		//hola chikis 
 	}
+	public Player(String name,String className,Integer health){
+		this.health=health;
+		this.name=name;
+		this.className=className;
+	}
+
+
+
+	public String getName() {
+		return this.name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getClassName() {
+		return this.className;
+	}
+
+
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+
+
+	public Integer getHealth() {
+		return this.health;
+	}
+
+
+
+	public void setHealth(Integer health) {
+		this.health = health;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Player [name= " + this.name + ", class= " + this.className  + ", health= " + this.className + " ]";
+	}
+	
 	
 
 }
